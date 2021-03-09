1d391c171370903d84217c2f907a8f490992c8ab
DylanHojnoski <dylanhoj@gmail.com>
2021-03-09 15:01:10 -0500

    Added book adding functionality

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+let myLibrary = [];
+let active = false;
+
+const body = document.querySelector("body");
+body.addEventListener("load", addBookToLibrary());
+
 function Book(title, author, pages, read)
 {
     this.title = title;
@@ -8,4 +14,88 @@ function Book(title, author, pages, read)
     {
         return title + " by " + author + ", " + pages + " pages, " + read;
     }
+}
+
+function addBookToLibrary()
+{
+    const addBookButton = document.querySelector("#addBook");
+    addBookButton.addEventListener("click", () => {
+        if(!active)
+        {
+            newBookForm();
+            active = true;
+        }
+    });
+}
+
+function newBookForm()
+{
+    const header = document.querySelector("header");
+    const bookForm = document.createElement("div");
+
+    const titleInput = document.createElement("input");
+    titleInput.setAttribute("type", "text");
+    titleInput.setAttribute("placeholder", "Title");
+
+    const authorInput = document.createElement("input");
+    authorInput.setAttribute("type", "text");
+    authorInput.setAttribute("placeholder", "Author");
+
+    const pagesInput = document.createElement("input");
+    pagesInput.setAttribute("type", "number");
+    pagesInput.setAttribute("placeholder", "Pages");
+
+    const readInput = document.createElement("input");
+    readInput.setAttribute("type", "checkbox");
+
+    const submit = document.createElement("button");
+    submit.innerText = "Add";
+    submit.addEventListener("click", () => {
+        myLibrary.push(new Book(titleInput.value, authorInput.value, pagesInput.value, readInput.checked));
+        bookForm.remove();
+        addBookCard();
+        active = false;
+    });
+    submit.textContent = "";
+
+    bookForm.append(titleInput);
+    bookForm.append(authorInput);
+    bookForm.append(pagesInput);
+    bookForm.append(readInput);
+    bookForm.append(submit);
+    header.append(bookForm);
+}
+
+function addBookCard()
+{
+    const bookcontainer = document.querySelector(".bookContainer");
+    const book = document.createElement("div");
+    book.setAttribute("class", "book");
+    const bookObject = myLibrary[myLibrary.length -1];
+
+    const title = document.createElement("h2");
+    title.textContent = bookObject.title;
+
+    const author = document.createElement("p");
+    author.textContent = `Author: ${bookObject.author}`;
+
+    const pages = document.createElement("p");
+    pages.textContent = `Pages: ${bookObject.pages}`;
+
+    const read = document.createElement("input");
+    read.setAttribute("checked", bookObject.read);
+    read.setAttribute("type", "checkbox");
+    read.addEventListener("change", () => {
+        bookObject.read = read;
+    });
+
+    const label = document.createElement("label");
+    label.textContent = "Read";
+
+    book.append(title);
+    book.append(author);
+    book.append(pages);
+    book.append(read);
+    book.append(label);
+    bookcontainer.append(book);
 }
